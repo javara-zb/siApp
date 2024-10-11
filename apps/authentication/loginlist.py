f5abee4cf5aac83eaf646a31f82de1276d72059e
javara-zb <126093853+javara-zb@users.noreply.github.com>
2024-10-11 09:42:23 -0400

    coalesce readme

--- apps/authentication/loginlist.py
+++ apps/authentication/loginlist.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('db.sqlite3')
 
 c = conn.cursor()
 
-c.execute('SELECT * FROM Users.db') 
+c.execute('SELECT * FROM sqlite_schema') 
 # retrieve all the data from the users table 
 users = c.fetchall() 
 
