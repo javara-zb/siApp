de27ca343c3a4342cd52f2ff9e9478dc6c9e4797
javara-zb <126093853+javara-zb@users.noreply.github.com>
2024-10-11 09:21:01 -0400

    look for a list of logins DRAFT

---
+++ apps/authentication/loginlist.py
@@ -0,0 +1,12 @@
+import sqlite3 
+
+conn = sqlite3.connect('db.sqlite3')
+
+c = conn.cursor()
+
+c.execute('SELECT * FROM Users.db') 
+# retrieve all the data from the users table 
+users = c.fetchall() 
+
+
+print(users)
