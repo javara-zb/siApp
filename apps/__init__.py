d158f66abb960c12b3642dda2c6f2700a5039b80
Zachary Bonta <85248478+zbsouthern@users.noreply.github.com>
2024-11-04 19:06:21 -0500

    remove sqlitedb

--- apps/__init__.py
+++ apps/__init__.py
@@ -9,7 +9,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
-
+from apps import db
 
 db = SQLAlchemy()
 login_manager = LoginManager()
